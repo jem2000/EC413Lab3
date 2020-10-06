1112703de015730010d855ad666f5cb4ae51c2aa
Justin Melville <justinem2000@gmail.com>
2020-10-06 18:47:31 -0400

    Fixed 4bit FA

--- fourbit_FA_str.v
+++ fourbit_FA_str.v
@@ -22,16 +22,17 @@
 
 module fourbit_FA_str(S, Cout, A, B, Cin);
 
-input [3:0] A, B, Cin;
+input [3:0] A, B;
+input Cin;
 output [3:0] S;
 output Cout;
 
 wire w1, w2, w3;
 
-FA_str      FA1     (w1, S[0], A[0], B[0], Cin[0]);
-FA_str      FA2     (w2, S[1], A[1], B[1], Cin[1]);
-FA_str      FA3     (w3, S[2], A[2], B[2], Cin[2]);
-FA_str      FA4     (Cout, S[3], A[3], B[3], Cin[3]);
+FA_str      FA1     (w1, S[0], A[0], B[0], Cin);
+FA_str      FA2     (w2, S[1], A[1], B[1], w1);
+FA_str      FA3     (w3, S[2], A[2], B[2], w2);
+FA_str      FA4     (Cout, S[3], A[3], B[3], w3);
 
 endmodule
 
--- fourbit_FA_str_tb.v
+++ fourbit_FA_str_tb.v
@@ -25,8 +25,7 @@ module fourbit_FA_str_tb;
 	wire Cout;
 	wire [3:0] S;
 	// Inputs
-	reg  A[3:0];
-	reg  B[3:0];
+	reg  [3:0] A, B;
 	reg Cin;
 	reg clk;
 	
@@ -34,8 +33,10 @@ module fourbit_FA_str_tb;
 	fourbit_FA_str fourbit_FA (
 		.Cout(Cout), 
 		.S(S), 
-        .A(A[0]), .A(A[1]), .A(A[2]), .A(A[3]), 
-		.B(B[0]), .B(B[1]), .B(B[2]), .B(B[3]),  
+        //.A(A[0]), .A(A[1]), .A(A[2]), .A(A[3]), 
+		//.B(B[0]), .B(B[1]), .B(B[2]), .B(B[3]),  
+		.A(A),
+		.B(B),
 		.Cin(Cin)
 	);
 	
@@ -47,11 +48,18 @@ module fourbit_FA_str_tb;
 		A[0] = 0; A[1] = 0;	A[2] = 0; A[3] = 0;
 		B[0] = 0; B[1] = 0; B[2] = 0; B[3] = 0;
 		Cin = 1;
+		
+		#20 A = 4'b0000; B = 4'b1010; Cin = 0;
+		#40 A = 4'b1111; B = 4'b1111; Cin = 0;
+		#60 A = 4'b1111; B = 4'b1111; Cin = 1;
+		#80 A = 4'b1010; B = 4'b1110; Cin = 0;
+		#100 A = 4'b1010; B = 4'b1110; Cin = 1;
 	end
 	
 	always @ (posedge clk)
 	begin
-		#100 {A[0], A[1], A[2], A[3], B[0], B[1], B[2], B[3], Cin} = {A[0], A[1], A[2], A[3], B[0], B[1], B[2], B[3], Cin} + 1;
+		#100 {B[0], B[1], B[2], B[3], Cin} = {B[0], B[1], B[2], B[3], Cin} + 1;
+		#100 {A[0], A[1], A[2], A[3]} = {A[0], A[1], A[2], A[3]} + 1;
 	end
   
 endmodule
